4af98924b5c5df6e5d7999c8aabecfb853a082ad
unknown <taphu0812@gmail.com>
2022-07-25 01:02:35 +0700

    upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+       Date date = new Date();
+        System.out.println("Now is :" + date);
+    }
+}
